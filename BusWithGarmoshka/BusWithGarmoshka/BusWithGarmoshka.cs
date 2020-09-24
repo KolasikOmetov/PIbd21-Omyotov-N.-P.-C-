4de7464baefa00c16ad9d1f28d8eb45875e4a349
KolasikOmetov <42410594+KolasikOmetov@users.noreply.github.com>
2020-09-24 08:44:45 +0300

    Lab 1 implementation

---
+++ BusWithGarmoshka/BusWithGarmoshka/BusWithGarmoshka.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusWithGarmoshka
+{
+    class BusWithGarmoshka
+    {
+        private float _startPosX;
+        private float _startPosY;
+        private int _pictureWidth;
+        private int _pictureHeight;
+        private readonly int carWidth = 320;
+        private readonly int carHeight = 60;
+
+        public int MaxSpeed { private set; get; }
+        public float Weight { private set; get; }
+        public Color MainColor { private set; get; }
+        public Color DopColor { private set; get; }
+        public bool BackDoors { private set; get; }
+        public bool Garmoshka { private set; get; }
+
+        public BusWithGarmoshka(int maxSpeed, float weight, Color mainColor, Color dopColor, bool backDoors, bool garmoshka)
+        {
+            MaxSpeed = maxSpeed;
+            Weight = weight;
+            MainColor = mainColor;
+            DopColor = dopColor;
+            BackDoors = backDoors;
+            Garmoshka = garmoshka;
+        }
+
+        public void SetPosition(int x, int y, int width, int height)
+        {
+            _startPosX = x;
+            _startPosY = y;
+            _pictureWidth = width;
+            _pictureHeight = height;
+        }
+
+        public void MoveTransport(Direction direction)
+        {
+            float step = MaxSpeed * 100 / Weight;
+            switch (direction)
+            {
+                // вправо
+                case Direction.Right:
+                    if (_startPosX + step < _pictureWidth - carWidth)
+                    {
+                        _startPosX += step;
+                    }
+                    break;
+                //влево
+                case Direction.Left:
+                    if (_startPosX - step > 0)
+                    {
+                        _startPosX -= step;
+                    }
+                    break;
+                //вверх
+                case Direction.Up:
+                    if (_startPosY - step > 0)
+                    {
+                        _startPosY -= step;
+                    }
+                    break;
+                //вниз
+                case Direction.Down:
+                    if (_startPosY + step < _pictureHeight - carHeight)
+                    {
+                        _startPosY += step;
+                    }
+                    break;
+            }
+        }
+
+        public void DrawTransport(Graphics g)
+        {
+
+            Brush cusov = new SolidBrush(MainColor);
+            g.FillRectangle(cusov, _startPosX, _startPosY, 150, 50);
+            Brush door = new SolidBrush(DopColor);
+            g.FillRectangle(door, _startPosX, _startPosY + 5, 10, 25);
+            g.FillRectangle(door, _startPosX, _startPosY + 40, 5, 5);
+            g.FillRectangle(door, _startPosX + 25, _startPosY + 5, 25, 40);
+            if (BackDoors)
+            {
+                g.FillRectangle(door, _startPosX + 100, _startPosY + 5, 25, 40);
+            }
+            Brush pen = new SolidBrush(Color.Black);
+            g.FillEllipse(pen, _startPosX + 5, _startPosY + 40, 20, 20);
+            g.FillEllipse(pen, _startPosX + 120, _startPosY + 40, 20, 20);
+            if (Garmoshka)
+            {
+                g.FillRectangle(pen, _startPosX + 150, _startPosY, 20, 50);
+
+                g.FillRectangle(cusov, _startPosX + 170, _startPosY, 150, 50);
+
+                g.FillRectangle(door, _startPosX + 195, _startPosY + 5, 25, 40);
+
+                if (BackDoors)
+                {
+                    g.FillRectangle(door, _startPosX + 270, _startPosY + 5, 25, 40);
+                }
+
+                g.FillEllipse(pen, _startPosX + 175, _startPosY + 40, 20, 20);
+                g.FillEllipse(pen, _startPosX + 290, _startPosY + 40, 20, 20);
+            }
+        }
+    }
+
+}
---
+++ BusWithGarmoshka/BusWithGarmoshka/FormBusWithGarmoshka.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BusWithGarmoshka
+{
+    public partial class FormBusWithGarmoshka : Form
+    {
+        private BusWithGarmoshka bus;
+
+        public FormBusWithGarmoshka()
+        {
+            InitializeComponent();
+        }
+
+        private void Draw()
+        {
+            Bitmap bmp = new Bitmap(pictureBoxBuses.Width, pictureBoxBuses.Height);
+            Graphics gr = Graphics.FromImage(bmp);
+            bus.DrawTransport(gr);
+            pictureBoxBuses.Image = bmp;
+        }
+
+        private void buttonCreate_Click(object sender, EventArgs e)
+        {
+            Random rnd = new Random();
+            bus = new BusWithGarmoshka(rnd.Next(100, 300), rnd.Next(1000, 2000), Color.Blue, Color.Yellow, true, true);
+            bus.SetPosition(rnd.Next(10, 100), rnd.Next(10, 100), pictureBoxBuses.Width, pictureBoxBuses.Height);
+            Draw();
+        }
+
+        private void buttonMove_Click(object sender, EventArgs e)
+        {
+            //получаем имя кнопки
+            string name = (sender as Button).Name;
+            switch (name)
+            {
+                case "buttonUp":
+                    bus.MoveTransport(Direction.Up);
+                    break;
+                case "buttonDown":
+                    bus.MoveTransport(Direction.Down);
+                    break;
+                case "buttonLeft":
+                    bus.MoveTransport(Direction.Left);
+                    break;
+                case "buttonRight":
+                    bus.MoveTransport(Direction.Right);
+                    break;
+            }
+            Draw();
+        }
+    }
+}
